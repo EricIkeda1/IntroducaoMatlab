72d04ef625a7ab7e8c8d22b0200e0593ad76d026
EricIkeda1 <ikedayuji.2002@gmail.com>
2026-03-17 21:17:30 -0300

    Função Degrau Bipolar

--- Aula 1/Atividade2.m
+++ Aula 1/Atividade2.m
@@ -20,4 +20,64 @@ hold on;
 plot(y1, 'g*');
 grid;
 ylim([-0.5 1.5]);
+legend('por código', 'por hardlim')
 title ('Função de Ativação - Degrau');
+
+%% Função Sigmoidal (Refatorado)
+
+clear; clc;
+
+% Vetor de entrada
+u = -2:0.01:2;
+
+% Intervalo de beta
+beta_min = 1;
+beta_max = 6;
+
+% Cores para cada curva
+cores = ['b', 'r', 'g', 'k', 'y', 'm'];
+
+figure(2);
+hold on;
+grid on;
+
+% Loop para cada valor de beta
+for beta = beta_min:beta_max
+    
+    % Função sigmoidal (vetorizada - sem for interno)
+    b = 1 ./ (1 + exp(-beta .* u));
+    
+    % Plot
+    plot(u, b, cores(beta), 'LineWidth', 1.5);
+end
+
+% Ajustes do gráfico
+ylim([-0.5 1.5]);
+title('Função de Ativação - Sigmoidal/logística');
+xlabel('u');
+ylabel('f(u)');
+
+% Legenda automática
+legend_strings = arrayfun(@(x) sprintf('beta=%d', x), beta_min:beta_max, 'UniformOutput', false);
+legend(legend_strings);
+
+%% Função Degrau Bipolar 
+clear; clc;
+
+% Vetor de entrada
+u = -2:0.01:2;
+
+g = zeros(size(u));
+
+g(u > 0) = 1;
+g(u < 0) = -1;
+
+figure(3)
+plot(u, g, 'k', 'LineWidth', 1.5);
+grid on;
+
+ylim([-1.5 1.5]);
+title('Função de Ativação - Degrau Bipolar');
+xlabel('u');
+ylabel('g(u)');
+
